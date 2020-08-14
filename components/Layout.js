30c1b233e3f75c72a97fbf935b97c14fe911aaf2
Jordy Yeoman <Jordyyeoman@gmail.com>
2020-08-15 01:38:05 +0800

    Added in mobile Head

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,15 @@
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
+import Head from 'next/head';
 
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col h-screen justify-between">
+      <Head>
+        <title>Draxen</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+      </Head>
       <Navigation />
       <div className="content-wrapper">{children}</div>
       <Footer />
