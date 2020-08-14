1e2e9a01096d86291f8b11f9981b0c03787c15fb
Jordy Yeoman <Jordyyeoman@gmail.com>
2020-08-14 12:47:42 +0800

    Added functioning email sign up form

--- pages/contact.js
+++ pages/contact.js
@@ -1,87 +1,133 @@
-import { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const Contact = () => {
-  const [hello, setHello] = useState('hi');
+  // 1. Create a reference to the input so we can fetch/clear it's value.
+  const inputEl = useRef(null);
+  const inputEl2 = useRef(null);
+  const inputEl3 = useRef(null);
+  // 2. Hold a message in state to handle the response from our API.
+  const [message, setMessage] = useState('');
+
+  const subscribe = async e => {
+    e.preventDefault();
+    // 3. Send a request to our API with the user's email address.
+    const res = await fetch('/api/subscribe', {
+      body: JSON.stringify({
+        email: inputEl.current.value,
+        fname: inputEl2.current.value,
+        lname: inputEl3.current.value,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+
+    const { error } = await res.json();
+
+    if (error) {
+      // 4. If there was an error, update the message in state.
+      setMessage(error);
+
+      return;
+    }
+
+    // 5. Clear the input value and show a success message.
+    inputEl.current.value = '';
+    inputEl2.current.value = '';
+    inputEl3.current.value = '';
+    setMessage('Success! 🎉 Welcome to the team legend! 🔥');
+  };
 
   return (
-    <div class="m-auto flex justify-center mt-20 pb-20">
-      <form class="w-5/6 md:w-full max-w-lg">
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+    <div className="m-auto flex flex-col items-center  justify-center mt-20 pb-20">
+      <div class="w-full flex justify-center md:w-3/5 lg:w-2/5 py-6 text-center">
+        <img class="w-2/3 z-50" src="/assets/draxenlogowtxt.png" />
+      </div>
+      <form className="w-5/6 md:w-full max-w-lg" onSubmit={subscribe}>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-first-name"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-first-name"
             >
               First Name
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
               id="grid-first-name"
               type="text"
               placeholder="Billy"
+              ref={inputEl2}
             />
-            <p class="text-red-500 text-xs italic">
+            <p className="text-red-500 text-xs italic">
               Please fill out this field.
             </p>
           </div>
-          <div class="w-full md:w-1/2 px-3">
+          <div className="w-full md:w-1/2 px-3">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-last-name"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-last-name"
             >
               Last Name
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-last-name"
               type="text"
+              ref={inputEl3}
               placeholder="Blogs"
             />
           </div>
         </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-password"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-password"
             >
               E-mail
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="email"
               type="email"
+              ref={inputEl}
             />
-            <p class="text-gray-600 text-xs italic"></p>
+            <p className="text-gray-600 text-xs italic"></p>
           </div>
         </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
+        {/* <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-password"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-password"
             >
               Message
             </label>
             <textarea
-              class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+              className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
               id="message"
             ></textarea>
-            <p class="text-gray-600 text-xs italic">
+            <p className="text-gray-600 text-xs italic">
               Enter any business related queries above.
             </p>
           </div>
-        </div>
-        <div class="md:flex md:items-center">
-          <div class="md:w-1/3">
+        </div> */}
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3">
             <button
-              class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-              type="button"
+              className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="submit"
             >
               Send
             </button>
           </div>
-          <div class="md:w-2/3"></div>
+          <div className="md:w-2/3">
+            {message
+              ? message
+              : `I'll only send emails when new content is posted. No spam.`}
+          </div>
         </div>
       </form>
     </div>
